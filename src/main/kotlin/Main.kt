cdb9ed25476110487ddae0e172a72599c4e64a7c
HEJ <aabc88@nate.com>
2025-03-19 18:00:03 +0900

    4,5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -42,6 +42,59 @@ class Solution3 {
     }
 }
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/120831
+//짝수의 합
+class Solution4 {
+    fun solution(n: Int): Int {
+        var answer = 0
+        for (n in 1..n) {
+            if (n % 2 != 1) {
+                answer += n
+            }
+        }
+        return answer
+    }
+}
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120898
+//편지
+class Solution5 {
+    fun solution(message: String): Int {
+        return (message.length) * 2
+    }
+}
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/134240
+//푸드파이터
+class Solution6 {
+    fun solution(food: IntArray): String {
+        var answer: String = ""
+        for (i in 1..food.size - 1) {
+            val j = food[i] / 2
+            if (j > 0) {
+                for (k in 1..j) {
+                    answer += i
+                }
+            }
+        }
+        val tmp = '0'+answer.reversed()
+        answer += tmp
+
+        return answer
+    }
+}
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/132267
+//콜라
+class Solution {
+    fun solution(a: Int, b: Int, n: Int): Int {
+        var answer: Int = 0
+        Math.ceil(1.5)
+        return answer
+    }
+}
+
+
 fun main() {
     //println(Solution().solution("one4seveneight")) //1
 
@@ -49,5 +102,22 @@ fun main() {
     /*val a : Array<String> = arrayOf("sun", "bed", "car")
     println(Solution().solution(a, 1))*/ //2
 
+    //println(Solution().solution(4))
+
+    val solution = Solution()
+    println(solution.solution(intArrayOf(1, 3, 4, 6)))
+    println(solution.solution(intArrayOf(1, 7, 1, 2)))
 
 }
+
+
+/* 양식
+//
+//
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        return answer
+    }
+}
+*/
